0d8342ac9ea115a5991d054fd1b75e28181d12f2
jvmoraiscb <jv.moraiscb@gmail.com>
2023-03-27 08:39:05 -0700

    :recycle: refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,22 @@
 #include "../include/myFalcon.h"
 
 int main(int argc, char* argv[]) {
-    libnifalcon::FalconDevice falcon;
+  libnifalcon::FalconDevice falcon;
 
-    if (!initialise(&falcon))
-        return 1;
+  if (!initialise(&falcon))
+    return 1;
 
-    myFalcon output = myFalcon(&falcon);
+  myFalcon output = myFalcon(&falcon);
 
-    printf("Calibrating...\n");
-    output.calibrate(&falcon);
-    while (true) {
-        double x, y, z;
-        int button1, button2, button3, button4;
-        output.set(1, -1, 1, -1, 1, -0.3);
-        output.start(&falcon);
-        output.get(&x, &y, &z, &button1, &button2, &button3, &button4);
-        printf("X: %.2lf | Y:%.2lf | Z: %.2lf | b1: %d | b2: %d | b3: %d | b4: %d\n", x, y, z, button1, button2, button3, button4);
-    }
+  while (true) {
+    double x, y, z;
+    int button1, button2, button3, button4;
 
-    return 0;
+    output.update(&falcon);
+    output.get(&x, &y, &z, &button1, &button2, &button3, &button4);
+
+    printf("X: %.2lf | Y:%.2lf | Z: %.2lf | b1: %d | b2: %d | b3: %d | b4: %d\n", x, y, z, button1, button2, button3, button4);
+  }
+
+  return 0;
 }
