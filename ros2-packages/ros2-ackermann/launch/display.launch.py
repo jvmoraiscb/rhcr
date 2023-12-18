914d54865420bb88c780ce5e627e5eb53a906f0d
jvmoraiscb <jv.moraiscb@gmail.com>
2023-12-18 12:42:04 -0300

    :heavy_check_mark: add ros2-ackermann custom package

--- ros2-packages/ros2-ackermann/launch/display.launch.py
+++ ros2-packages/ros2-ackermann/launch/display.launch.py
@@ -17,13 +17,6 @@ def generate_launch_description():
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
@@ -34,14 +27,11 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node
     ])
