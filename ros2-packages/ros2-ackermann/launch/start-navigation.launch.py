bbf8c7a87236fe3e7d33ce38429b62406c242259
jvmoraiscb <jv.moraiscb@gmail.com>
2024-01-08 10:44:50 -0300

    :heavy_check_mark: add nav2 inside launch file

--- ros2-packages/ros2-ackermann/launch/start-navigation.launch.py
+++ ros2-packages/ros2-ackermann/launch/start-navigation.launch.py
@@ -4,10 +4,12 @@ import launch_ros
 import os
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='ros2-ackermann').find('ros2-ackermann')
-    model_path = os.path.join(pkg_share, 'src/description/ackermann_description.urdf')
-    rviz_config_path = os.path.join(pkg_share, 'config/rviz.config.rviz')
-    slam_config_path = os.path.join(pkg_share, 'config/slam_toolbox.config.yaml')
+    my_pkg_share = launch_ros.substitutions.FindPackageShare(package='ros2-ackermann').find('ros2-ackermann')
+    nav2_pkg_share = launch_ros.substitutions.FindPackageShare(package='nav2_bringup').find('nav2_bringup')
+    model_path = os.path.join(my_pkg_share, 'src/description/ackermann_description.urdf')
+    rviz_config_path = os.path.join(my_pkg_share, 'config/rviz.config.rviz')
+    slam_config_path = os.path.join(my_pkg_share, 'config/slam_toolbox.config.yaml')
+    nav2_launch_path = os.path.join(nav2_pkg_share, 'launch/navigation_launch.py')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
@@ -34,13 +36,6 @@ def generate_launch_description():
         executable='static_transform_publisher',
         arguments = ['--x', '0', '--y', '0', '--z', '0', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'map', '--child-frame-id', 'odom']
     )
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', rviz_config_path],
-    )
     slam_node = launch_ros.actions.Node(
         parameters=[
           slam_config_path,
@@ -50,6 +45,18 @@ def generate_launch_description():
         executable='async_slam_toolbox_node',
         name='slam_toolbox'
     )
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_path],
+    )
+    navigation_launch = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            nav2_launch_path
+        )
+    )
 
     return launch.LaunchDescription([
         joint_state_publisher_node,
@@ -57,5 +64,6 @@ def generate_launch_description():
         ackermann_node,
         tf2_node,
         slam_node,
+        navigation_launch,
         rviz_node
     ])
