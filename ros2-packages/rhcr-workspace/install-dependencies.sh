57cd365bbd64da26301d0dd5603db44a37973866
João Victor Morais <jv.moraiscb@gmail.com>
2024-03-25 00:38:23 -0300

    :recycle: refactor folders

--- ros2-packages/rhcr-workspace/install-dependencies.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install -y \
-python3-colcon-common-extensions \
-ros-humble-joint-state-publisher \
-ros-humble-xacro \
-ros-humble-slam-toolbox \
-ros-humble-navigation2 \
-ros-humble-nav2-bringup \
